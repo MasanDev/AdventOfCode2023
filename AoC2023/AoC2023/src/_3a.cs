ef606fa79fe17a97d70c3e5b974f651a9106843a
matsa979 <matsa979@student.liu.se>
2023-12-03 09:19:36 +0100

    3a commit

---
+++ AoC2023/AoC2023/src/_3a.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2023.src
+{
+    public class _3a
+    {
+        public _3a() {
+            Console.WriteLine("public");
+                
+        }
+    }
+}
